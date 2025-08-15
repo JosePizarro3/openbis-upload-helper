dc6986ee9b71380e2eeb1fd78b939dc0f56c9d45
jpizarro <jose.pizarro-blanco@bam.de>
2025-08-15 15:32:09 +0200

    Restore path to pkg
    Bumped dependency of bam-masterdata

--- openbis_upload_helper/manage.py
+++ openbis_upload_helper/manage.py
@@ -7,9 +7,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE", "openbis_upload_helper.uploader.settings"
-    )
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "uploader.settings")
     try:
         from django.core.management import execute_from_command_line  # noqa: PLC0415
     except ImportError as exc:
