fba9cf2023a971677810712cc96ac8e0db2e1db1
Lucas Zimmermann <lucas.zimmermann@bam.de>
2025-09-01 14:13:35 +0200

    removed unused files

--- OpenBISUploadHelper.bat
+++ OpenBISUploadHelper.bat
@@ -1,3 +1,17 @@
 @echo off
-call .\venv\Scripts\activate.bat & python OpenBISUploadHelper.py
+set "VENV_DIR=%~dp0 .venv"
+set
+
+REM Check if venv env exists
+if exist "%VENV_DIR%\Scripts\activate.bat" (
+    call "%VENV_DIR%\Scripts\activate.bat"
+) else (
+    echo
+    REM activate conda env
+    call conda activate "%VENV_DIR%"
+)
+
+REM Start Server
+python openbis_upload_helper/manage.py runserver
+
 @echo on
